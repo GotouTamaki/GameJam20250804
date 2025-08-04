00bb749250f5ad26d488c04e2b0677fd6dbf5e8c
flaming8634 <flaming.8634@icloud.com>
2025-08-04 15:15:14 +0900

    [add]SushiMoveを追加

---
+++ Assets/Scripts/Sushi/SushiMove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class SushiMove : MonoBehaviour
+{
+    [SerializeField, Range(0.1f, 10)] public float SushiMoveSpeed = 3f;
+    [SerializeField] public Vector3 MoveDirection = Vector3.left;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(MoveDirection.normalized * SushiMoveSpeed *  Time.deltaTime);
+    }
+}
