9ed454f87ab62f702ef95b71f6eb9f6b53588c72
マックス <maxoumixou@gmail.com>
2025-08-04 13:09:40 +0900

    タイトル画面のUI、シーン移動、音楽マネージャー

---
+++ Assets/Scripts/Managers/SceneChangeManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChangeManager : MonoBehaviour
+{
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene("TitleMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
