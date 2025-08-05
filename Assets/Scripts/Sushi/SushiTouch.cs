181cd53e26fe9f5158ed7c5e9f22f1f1db54308c
GotouTamaki <tamagoro0226@gmail.com>
2025-08-05 14:47:02 +0900

    [add]寿司をクリックした時の挙動とデータを追加

---
+++ Assets/Scripts/Sushi/SushiTouch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+public class SushiTouch : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Sprite _clickSprite;
+    [SerializeField] private float _destroyDelayTime = 0.5f;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void OnEnable()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("ﾐ゜ｯ");
+        SoundManager.Instance.PlayShootSFX();
+
+        if (_clickSprite is not null)
+        {
+            _spriteRenderer.sprite = _clickSprite;
+        }
+
+        Destroy(gameObject, 0.5f);
+    }
+}
